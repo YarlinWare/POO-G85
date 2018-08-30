e5e06f3dd6a5d826464086163f4f50c91de22217
Juan Felipe Herrera Poveda <jfherrerap@correo,udistrital.edu.co>
2018-08-29 22:44:29 -0500

    Se añade función reversa

--- Cronometro/src/cronometro/Cronometro.java
+++ Cronometro/src/cronometro/Cronometro.java
@@ -19,23 +19,27 @@ public class Cronometro {
         Tiempo time = new Tiempo();
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese la opción de su elección: ");
-        System.out.println("1. Iniciar desde cero ");
-        System.out.println("2. Iniciar desde un tiempo en especifico "); 
-        System.out.println("3. Iniciar de manera regresiva desde un tiempo en especifico ");   
-        System.out.println("4. Iniciar y detener en... ");
+        System.out.println("1. Iniciar desde cero hacia adelante ");
+        System.out.println("2. Iniciar desde cero hacia atras ");
+       // System.out.println("3. Iniciar desde cero hasta un numero indicado y luego retroceder ");
+    // System.out.println("4. Iniciar desde un tiempo en especifico "); 
+       // System.out.println("3. Iniciar de manera regresiva desde un tiempo en especifico ");   
+       // System.out.println("4. Iniciar y detener en... ");
         int option=sc.nextInt();
         
         switch (option) {
             case 1:  time.Iniciar();
                      break;
-            case 2:  
-                System.out.println("Ingrese Hora: ");
-                int h=sc.nextInt();
-                System.out.println("Ingrese Minuto: ");
-                int m=sc.nextInt();
-                System.out.println("Ingrese Segundo: ");
-                int s=sc.nextInt();
-                time.Reanudar(h, m, s);
+            case 2: time.Reversa();
+                    break;
+            case 3:  
+                    System.out.println("Ingrese Hora: ");
+                    int h=sc.nextInt();
+                    System.out.println("Ingrese Minuto: ");
+                    int m=sc.nextInt();
+                    System.out.println("Ingrese Segundo: ");
+                    int s=sc.nextInt();
+                    time.Reanudar(h, m, s);
                      break;            
             default: 
                      break;
--- Cronometro/src/cronometro/Tiempo.java
+++ Cronometro/src/cronometro/Tiempo.java
@@ -54,11 +54,15 @@ public class Tiempo {
     }
     
     public void Reanudar(int h, int m, int s)throws InterruptedException {
-        hora=h;
-        minuto=m;
-        segundo=s;        
+        hora=0;
+        minuto=0;
+        segundo=0; 
+        decima=0;
+        centesima=0;
+        milesima=0;
+        diezmilesima=0;
         long start = System.currentTimeMillis();
-        while(diezmilesima>-1){
+        while(hora!=h&&minuto!=m&&segundo!=s){
         if(diezmilesima==9 ){
             Thread.sleep(2);
             diezmilesima=0;
@@ -96,10 +100,65 @@ public class Tiempo {
             }
             diezmilesima ++;
             System.out.println(hora+":"+minuto+":"+segundo+":"+""+decima+""+centesima+""+milesima+""+diezmilesima);
-        }    
+        } 
+        if(hora==h&&minuto==m&&segundo==s)
+        {
+            while(hora!=0&&minuto!=0&&segundo!=0);
+        {
+                if(hora==0)
+		{
+			hora=23;
+			minuto=59;
+			segundo=59;
+                        decima=9;
+                        centesima=9;
+                        milesima=9;
+                        diezmilesima=9;
+		}
+		
+                Thread.sleep(0,1);
+                diezmilesima--;
+                if(diezmilesima==0)
+		{
+			milesima--;
+			diezmilesima=9;	
+		}	
+                if(milesima==0)
+		{
+			centesima--;
+			milesima=9;	
+		}	
+                if(centesima==0)
+		{
+			decima--;
+			centesima=9;	
+		}	
+                if(decima==0)
+		{
+			segundo--;
+			decima=9;	
+		}	
+
+
+		if(segundo==0)
+		{
+			minuto=minuto-1;
+			segundo=59;	
+		}	
+		if(minuto==0)
+		{
+			hora=hora-1;
+			minuto=59;
+		}
+		
+                
+            System.out.println(hora+":"+minuto+":"+segundo+":"+":"+decima+":"+centesima+":"+milesima+":"+diezmilesima);
+
+        }
+        }
     }
     
-    public void Reanudar()throws InterruptedException{
+    public void Reanudar2()throws InterruptedException{
         hora =hora;
         minuto=minuto;
         segundo=segundo;
@@ -165,7 +224,62 @@ public class Tiempo {
         
     }
     public void Detener(){
-        System.out.println(hora+":"+minuto+":"+segundo+":"+""+decima+""+centesima+""+milesima+""+diezmilesima);        
+        System.out.println(hora+":"+minuto+":"+segundo+":"+":"+decima+":"+centesima+":"+milesima+":"+diezmilesima);        
     }
-    
+    public void Reversa() throws InterruptedException
+    {
+        long start = System.currentTimeMillis();
+        while(true)
+        {
+                if(hora==0)
+		{
+			hora=23;
+			minuto=59;
+			segundo=59;
+                        decima=9;
+                        centesima=9;
+                        milesima=9;
+                        diezmilesima=9;
+		}
+		
+                Thread.sleep(0,1);
+                diezmilesima--;
+                if(diezmilesima==0)
+		{
+			milesima--;
+			diezmilesima=9;	
+		}	
+                if(milesima==0)
+		{
+			centesima--;
+			milesima=9;	
+		}	
+                if(centesima==0)
+		{
+			decima--;
+			centesima=9;	
+		}	
+                if(decima==0)
+		{
+			segundo--;
+			decima=9;	
+		}	
+
+
+		if(segundo==0)
+		{
+			minuto=minuto-1;
+			segundo=59;	
+		}	
+		if(minuto==0)
+		{
+			hora=hora-1;
+			minuto=59;
+		}
+		
+                
+            System.out.println(hora+":"+minuto+":"+segundo+":"+":"+decima+":"+centesima+":"+milesima+":"+diezmilesima);
+
+        }
+    }
 }
